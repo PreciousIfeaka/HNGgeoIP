bd6583d957ed2e02903f114f00fef292a4c74497
PreciousIfeaka <preciousifeaka@gmail.com>
2024-07-01 16:50:10 +0100

    enabled express json

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const helloRouter = require("./router/hello.router");
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 
 const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true }));
 
 app.get("/", async (req, res) => {
   res.send("Use the complete query url");
